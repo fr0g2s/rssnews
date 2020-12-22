fc9099ad2f0d9fbfcb452d0ae58837db7100d7b2
fr0g2s <turttle2s@naver.com>
2020-12-21 21:33:31 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
-sudo apt install python3 python3-venv -y
-pip install feedparser Flask ssl
+sudo apt install python3 python3-venv python3-pip -y
+sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev -y
+pip3 install feedparser Flask ssl
