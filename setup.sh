1be1b25ac175ca7e9988666c619c371d52aad8fe
fr0g2s <jinjae1065@gmail.com>
2020-12-22 19:06:06 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 sudo apt install python3 python3-venv python3-pip -y
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev -y
-pip3 install feedparser Flask ssl
+pip3 install feedparser flask
+pip3 install ssl
