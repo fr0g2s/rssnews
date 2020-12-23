df0bbe4cf12fef7240b8e6613d2fe0fd8b9bda4f
fr0g2s <jinjae1065@gmail.com>
2020-12-23 11:25:39 -0800

    Update flaskr.py

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -13,16 +13,29 @@ PASSWORD = 'default'
 
 app = Flask(__name__)
 #app.config.from_object(__name__)
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+app.config.from_envvar("FLASKR_SETTINGS", silent=True)
 
 def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read())
+            db.cursor().executescript(f.read().decode('utf-8'))
         db.commit()
-
+    
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+@app.before_connect
+def before_connect():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    g.db.close()
+
+@app.after_connect
+def after_connect():
+    g.db.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1337)
