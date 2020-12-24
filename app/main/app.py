158e007cf5c4b7b7496dae5a97f2614efb3fe8e1
fr0g2s <jinjae1065@gmail.com>
2020-12-24 06:51:29 -0800

    Add app.py

---
+++ app/main/app.py
@@ -0,0 +1,47 @@
+from __future__ import with_statement
+from contextlib import closing
+from flask import Blueprint, request, render_template, flash, redirect, url_for
+from flask import current_app as app
+
+import sqlite3
+import rssparser
+import rssmanager
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def show_main():
+    return render_template('/index.html')
+
+@app.route('/add', method=['GET'])
+def add_rss(rss):
+    
+    pass
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('init_db.sql') as f:
+            db.cursor().executescript(f.read().decode('utf-8'))
+        db.commit()
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+@app.befre_connect()
+def before_connect():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    g.db.close()
+
+if __name__ == "__main__":
+    app.debug = True
+    app.config.from_object("config")
+    g.rssmanager = rssmanager.RssManager()
+    g.rssparser = rssparser.RssParser()
+    
+    init_db()
+
+    app.run(host="0.0.0.0", port=1337)
