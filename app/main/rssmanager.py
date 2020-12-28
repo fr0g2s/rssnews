5408dc789e7cf16de643e77e3b0f9bca0d5acf3e
fr0g2s <turttle2s@naver.com>
2020-12-28 00:19:33 -0800

    Update rssmanager.py

--- app/main/rssmanager.py
+++ app/main/rssmanager.py
@@ -6,7 +6,7 @@ class RssManager:
     def __init__(self):
         pass
 
-    def getArticles(self, db):   # 
+    def getArticles(self, db):    
         cur = db.execute('select author, title, description, url, published from articles order by id desc')
         articles = [dict(
                         author=row[0], 
@@ -15,37 +15,49 @@ class RssManager:
                         url=row[3], 
                         published=row[4]
                         ) for row in cur.fetchall()]
-        db.commit()
         return articles
 
-    def delRss(self, db, del_rss):  # delete rss from db
+    def getEntries(self, db):
+        cur = db.execute('select rss, author, published from entries')
+        entries = [dict(rss=row[0], author=row[1], published=row[2]) for row in cur.fetchall()]
+        return entries
+
+    def updateRss(self, db):    # update published in entries
+        cur = db.execute('select rss from entries')
+        rssentries = [dict(row[0]) for row in cur.fetchall()]
+        
+
+    def delRss(self, db, del_rss):  # delete rss from entries db
         if not self.__isExist(db, del_rss):
             raise Exception('given RSS is not exist in DB: ', del_rss)
-
-        db.execute('delete from articles where rss = ?', [ del_rss ])
+        db.execute('delete from entries where rss = ?', [ del_rss ])
         db.commit()
 
-    def editRss(self, db, old_rss, new_rss):
-        if self.__isExist(old_rss):
+    def editRss(self, db, old_rss, new_rss): # edit rss in entries db
+        if not self.__isExist(db, old_rss):
             raise Exception('old RSS is invalid: ', old_rss)
         if not self.__isValidRss(new_rss):
             raise Exception('new RSS is invalid: ', new_rss)
-        db.execute('update articles SET rss = ? where rss = ?', [ new_rss, old_rss ])
+        parsed = rssparser.RssParser().getParsedRss(new_rss)
+        db.execute('update entries set rss = ?, author = ?, published = ? where rss = ?', 
+                    [ new_rss, parsed['author'], parsed['published'], old_rss ])
         db.commit()
 
-    def addRss(self, db, new_rss):
+    def addRss(self, db, new_rss):  # add rss to entries db
         if self.__isExist(db, new_rss):
-            raise Exception('new RSS is already exist in DB: ', new_rss)
+            raise Exception('new RSS is already exist in DB(entries): ', new_rss)
         if not self.__isValidRss(new_rss):
             raise Exception('new RSS is invalid: ', new_rss)
         
         data = rssparser.RssParser().getParsedRss(new_rss)
-
-        db.execute('insert into articles (rss, author, url, title, description, published) values (?,?,?,?,?,?)', 
-                [ new_rss, data['author'], data['url'], data['title'], data['description'], data['published'] ])
-
+        if data != False:
+            db.execute('insert into entries (rss, author, published) values (?,?,?)', [ new_rss, data['author'], data['published'] ])
+            return True
+        else:   # wrong RSS
+            return False
+    
     def __isExist(self, db, rss):
-        cur = db.execute('select rss from articles where rss = ?', [ rss ])
+        cur = db.execute('select rss from entries where rss = ?', [ rss ])
         if cur.fetchone():
             return True
         return False
@@ -58,40 +70,3 @@ class RssManager:
             return False
         return True
 
-def test_interface_tail(rmgr):
-    print(rmgr.getArticles(db))
-    print('='*50)
-
-if __name__ == "__main__": # for test
-    rmgr = RssManager()
-    db = sqlite3.connect('/tmp/rssnews.db')
-    print('[*] First ')
-    test_interface_tail(rmgr)
-
-    print('[*] Delete RSS ')
-    try:
-        rmgr.delRss(db, 'https://rss.blog.naver.com/turttle2s.xml')
-    except Exception as e:
-        print('[-] RssManager.delRss Error: ', e)
-    test_interface_tail(rmgr)
-
-    print('[*] Del invalid rss ')
-    try:
-        rmgr.delRss(db, 'invlid.rss')
-    except Exception as e:
-        print('[-] RssManager.delRss Error: ', e)
-    test_interface_tail(rmgr)
-    
-    print('[*] Add new RSS ')
-    try:
-        rmgr.addRss(db, 'https://rss.blog.naver.com/turttle2s.xml')
-    except Exception as e:
-        print('[-] RssManager.addRss Error: ', e)
-    test_interface_tail(rmgr)
-    
-    print('[*] Add invalid RSS')
-    try:
-        rmgr.addRss(db, 'rss.blog.naver.com/turttle2s.xml')
-    except Exception as e:
-        print('[-] RssManager.addRss Error: ', e)
-    test_interface_tail(rmgr)
