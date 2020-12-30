ef59608c8783cc934513cb1465cc1d625f9fe286
fr0g2s <turttle2s@naver.com>
2020-12-30 10:36:56 -0800

    Remove main

--- app/main/init_db.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-sqlite3 /tmp/rssnews.db < init_articles.sql
-sqlite3 /tmp/rssnews.db < init_entries.sql
--- app/main/init_entries.sql
+++
@@ -1,6 +0,0 @@
-drop table if exists entries;
-create table entries (
-    rss string primary key null,
-    author string not null,
-    published string not null
-);
